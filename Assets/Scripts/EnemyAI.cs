d8f3f121b57e82848d4726747b4567949a9616a5
Skues <eyg22pfu@uea.ac.uk>
2025-02-17 22:34:05 +0000

    Enemy follows player if close

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,9 +1,15 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class FieldofView : MonoBehaviour
+public class EnemyAI : MonoBehaviour
 {
-    private float viewRadius = 10f;
+
+    public Transform[] points;
+    private int destPoint = 0;
+    private NavMeshAgent agent;
+    bool isChasing;
+    private float viewRadius = 15f;
     private float viewAngle = 120f;
     public GameObject player;
     public LayerMask obstacleMask;
@@ -12,6 +18,9 @@ public class FieldofView : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        agent = GetComponent<NavMeshAgent>();
+        agent.autoBraking = false;
+        GotoNextPoint();
         threshold = Mathf.Cos(viewAngle * 0.5f * Mathf.Deg2Rad);
     }
 
@@ -31,6 +40,8 @@ public class FieldofView : MonoBehaviour
                     RaycastHit hit;
                     if (!Physics.Raycast(transform.position, direction, out hit, distToTarget, obstacleMask))
                     {
+                        ChasePlayer();
+
                         print("PLAYER IN VIEW");
                         // if (hit.collider.gameObject == player)
                         // {
@@ -42,16 +53,36 @@ public class FieldofView : MonoBehaviour
                         //     Debug.Log("Player is in FOV but blocked by " + hit.collider.gameObject.name);
                         // }
                     }
-                    // else
-                    // {
-                    //     // No obstacle in the way, the player is visible
-                    //     Debug.Log("Player is in FOV and no obstacle detected");
-                    // }
+                    else if (isChasing && Vector3.Distance(transform.position, player.transform.position) > viewRadius)
+                    {
+                        StopChase();
+                    }
                 }
-
             }
         }
         
-        Debug.DrawRay(transform.position, transform.forward);
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            GotoNextPoint();
+    }
+
+    void ChasePlayer(){
+        isChasing = true;
+        agent.SetDestination(player.transform.position);
+    }
+    void StopChase(){
+        isChasing = false;
+        GotoNextPoint();
+    }
+    void GotoNextPoint() {
+        // Returns if no points have been set up
+        if (points.Length == 0)
+            return;
+
+            // Set the agent to go to the currently selected destination.
+        agent.destination = points[destPoint].position;
+
+            // Choose the next point in the array as the destination,
+            // cycling to the start if necessary.
+        destPoint = (destPoint + 1) % points.Length;
     }
 }
